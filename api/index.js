9b6a8f371964d2f95552d01fc3de5ff503842257
Gabrielle Freire <gabrielle.fpr@gmail.com>
2022-01-29 20:01:53 -0300

    Deploy

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ const app = express ()
 
 routes(app)
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
 app.listen(port, ()=> console.log(`server running on port ${port}`))
 
 module.exports = app

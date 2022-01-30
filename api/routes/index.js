3d27336c7cded65e8ae8695fed8224e943ac25f1
Gabrielle Freire <gabrielle.fpr@gmail.com>
2022-01-29 22:15:23 -0300

    Rotas e Portas

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,11 @@ const vagas = require('./vagasRoute')
 
 module.exports = app => {
     app.use(express.json())
+    app.use((req, res, next) => {
+        console.log(req.headers.host, new Date().toLocaleTimeString());
+        next();
+})
+
     app.use(usuarios)
     app.use(vagas)
 }
-
